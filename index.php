cf756a8ff0896f415827162af88b734ffd1930cf
psuuc-jbgarcia <gjeric54321@gmail.com>
2025-07-17 21:35:51 +0800

    "start of plattech"

--- index.php
+++ index.php
@@ -1,10 +1,38 @@
 <?php
-// File A.php
-namespace App\Models;
-class User {}
 
-// File B.php
-namespace App\Controllers;
-class User {}
+class Car {
+    public string $brand;
+    private int $year;
+    protected string $engine;
 
-?>
+    public function drive() {
+        echo "Driving...\n";
+    }
+
+    private function startEngine() {
+        echo "Starting engine...\n";
+    }
+
+    protected function stopEngine() {
+        echo "Stopping engine...\n";
+    }
+}
+
+// Create reflection class instance
+$ref = new ReflectionClass('Car');
+
+// Get properties
+$properties = $ref->getProperties();
+echo "Properties of class Car:\n";
+foreach ($properties as $prop) {
+    $visibility = implode(' ', Reflection::getModifierNames($prop->getModifiers()));
+    echo "- " . $prop->getName() . " ($visibility)\n";
+}
+
+// Get methods
+$methods = $ref->getMethods();
+echo "\nMethods of class Car:\n";
+foreach ($methods as $method) {
+    $visibility = implode(' ', Reflection::getModifierNames($method->getModifiers()));
+    echo "- " . $method->getName() . " ($visibility)\n";
+}
