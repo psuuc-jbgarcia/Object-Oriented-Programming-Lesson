1996dbfc78e8422664e400a8c73fd8d736561077
psuuc-jbgarcia <gjeric54321@gmail.com>
2025-07-21 17:06:55 +0800

    "done week 9 oop["

--- index.php
+++ index.php
@@ -1,39 +1,9 @@
 <?php
-
-class Car {
-    public string $brand;
-    private int $year;
-    protected string $engine;
-
-    public function drive() {
-        echo "Driving...\n";
-    }
-
-    private function startEngine() {
-        echo "Starting engine...\n";
-    }
-
-    protected function stopEngine() {
-        echo "Stopping engine...\n";
+class Helper {
+    public static function greet($name) {
+        return "Hello, $name!";
     }
 }
 
-// Create reflection class instance
-$ref = new ReflectionClass('Car');
-
-// Get properties
-$properties = $ref->getProperties();
-echo "Properties of class Car:\n";
-foreach ($properties as $prop) {
-    $visibility = implode(' ', Reflection::getModifierNames($prop->getModifiers()));
-    echo "- " . $prop->getName() . " ($visibility)\n";
-}
-
-// Get methods
-$methods = $ref->getMethods();
-echo "\nMethods of class Car:\n";
-foreach ($methods as $method) {
-    $visibility = implode(' ', Reflection::getModifierNames($method->getModifiers()));
-    echo "- " . $method->getName() . " ($visibility)\n";
-}
-
+// ✅ No need to create an object
+echo Helper::greet("Jerico");  // Output: Hello, Jerico!
