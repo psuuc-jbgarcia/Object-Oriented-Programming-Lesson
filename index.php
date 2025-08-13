a25e145433302c23c84c8fb85370f6b61b5fc1bb
psuuc-jbgarcia <gjeric54321@gmail.com>
2025-08-13 21:36:07 +0800

    add trim

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
-<? php
+<?php
 // Use PHPunit Framework
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
 
 
-// Extend the test case class of phpunit
-class StackTest extends TestCase
-{
-    public function testPushAndPop()
-   {
+// // // Extend the test case class of phpunit
+// // class StackTest extends TestCase
+// // {
+// //     public function testPushAndPop()
+// //    {
 
-// create an empty vector
-$vector = new \Ds\Vector();
+// // // create an empty vector
+// // $vector = new \Ds\Vector();
 
-// assert the size of vector
-$this->assertSame(0, count($vector));
-$vector->insert(0, "first");
-// assert the value of vector 
-$this->assertSame('first', $vector[count($vector)-1]);
+// // // assert the size of vector
+// // $this->assertSame(0, count($vector));
+// // $vector->insert(0, "first");
+// // // assert the value of vector 
+// // $this->assertSame('first', $vector[count($vector)-1]);
 
-// assert the size of vector
-$this->assertSame(1, count($vector));
+// // // assert the size of vector
+// // $this->assertSame(1, count($vector));
 
-// pop and assert the popped element
-$this->assertSame('first', $vector->pop());
-$this->assertSame(0, count($vector));
-    }
-}
+// // // pop and assert the popped element
+// // $this->assertSame('first', $vector->pop());
+// // $this->assertSame(0, count($vector));
+// //     }
+// // }
+// // 
+
+echo "Enter your name: ";
+$name = trim(fgets(STDIN));
+echo "Hello,$name";
 ?>
