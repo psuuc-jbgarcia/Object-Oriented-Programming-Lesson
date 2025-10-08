d1944235e5aa90c8cd3b5544600308ae5efe6dbf
psuuc-jbgarcia <gjeric54321@gmail.com>
2025-10-08 21:11:49 +0800

    "hmpp"

--- labexam.php
+++ labexam.php
@@ -1,53 +1,63 @@
 <?php
 
 abstract class ValorantPlayer {
-    protected $name;
+    protected $playerName;
     protected $kills;
+    protected $assists;
     protected $deaths;
+    protected $score = 0;
 
-    public function __construct($name, $kills, $deaths) {
-        $this->name = $name;
+    public function __construct($playerName, $kills, $assists, $deaths) {
+        $this->playerName = $playerName;
         $this->kills = $kills;
+        $this->assists = $assists;
         $this->deaths = $deaths;
     }
 
-    abstract protected function getKDRatio();
+    abstract public function calculateScore();
 
-    public function displayStats() {
-        $kd = number_format($this->getKDRatio(), 2);
-        echo "Name: {$this->name}\n";
-        echo "Kills: {$this->kills}\n";
-        echo "Deaths: {$this->deaths}\n";
-        echo "K/D Ratio: {$kd}\n";
+    public function showStats() {
+        echo "Player: {$this->playerName}\n";
+        echo "Kills: {$this->kills} | Assists: {$this->assists} | Deaths: {$this->deaths}\n";
+        echo "Score: {$this->score}\n";
+
+        if ($this->score > 100)
+            echo " MVP Caliber!\n\n";
+        else
+            echo "Keep Practicing!\n\n";
     }
 }
 
-class CasualPlayer extends ValorantPlayer {
-    protected function getKDRatio() {
-        return $this->deaths == 0 ? $this->kills : $this->kills / $this->deaths;
+class Player1 extends ValorantPlayer {
+    public function calculateScore() {
+        $this->score = ($this->kills * 3) + ($this->assists * 2) - ($this->deaths * 1);
     }
 }
 
-class ProPlayer extends ValorantPlayer {
-    protected function getKDRatio() {
-        return $this->deaths == 0 ? $this->kills : $this->kills / $this->deaths;
-    }
-
-    public function displayStats() {
-        echo "🔥 PRO PLAYER ALERT! 🔥\n";
-        parent::displayStats();
-        if ($this->getKDRatio() > 1.5) {
-            echo "K/D Ratio is impressive!\n";
-        }
+class Player2 extends ValorantPlayer {
+    public function calculateScore() {
+        $this->score = ($this->kills * 3) + ($this->assists * 2) - ($this->deaths * 1);
     }
 }
 
-// Example usage:
-$player1 = new CasualPlayer("Jett", 20, 10);
-$player2 = new ProPlayer("TenZ", 30, 10);
+class Player3 extends ValorantPlayer {
+    public function calculateScore() {
+        $this->score = ($this->kills * 3) + ($this->assists * 2) - ($this->deaths * 1);
+    }
+}
 
-$player1->displayStats();
-echo "\n";
-$player2->displayStats();
+// --- TEST AREA ---
+$player1 = new Player1("Jerico", 25, 5, 10);
+$player2 = new Player2("Patrick", 35, 12, 3);
+$player3 = new Player3("Miguel", 30, 8, 0);
+
+$player1->calculateScore();
+$player1->showStats();
+
+$player2->calculateScore();
+$player2->showStats();
+
+$player3->calculateScore();
+$player3->showStats();
 
 ?>
